a6e1aff8210536f6e0732249aeef40a2110f073f
newohrim <ohrim71@gmail.com>
2021-04-18 19:17:43 +0300

    optimized fragment shader no more conditions in shaders perfomance boost, almost equal to no-checkerboard mode perfomance still highly depends on screen resolution. i hate it. maybe going to add more cubes on scene

--- x64/Release/shaders/fbo_to_screenquad_fragment_shader.glsl
+++ x64/Release/shaders/fbo_to_screenquad_fragment_shader.glsl
@@ -33,10 +33,51 @@ void main()
 		texelFetch(prevScreenTexture, ivec2(gl_FragCoord.x + 1, gl_FragCoord.y - 1), 0)
 	));*/
 	
-	if (isEvenFrame) {
-		if (mod(floor(gl_FragCoord.y), 2) == 1)
+	bool x_even = bool(int(gl_FragCoord.x) & 1);
+	bool y_even = bool(int(gl_FragCoord.y) & 1);
+	
+	//int x_even = int(gl_FragCoord.x) & 1;
+	//int y_even = int(gl_FragCoord.y) & 1;
+	
+	vec4 outColor1 = texelFetch(screenTexture, ivec2(gl_FragCoord.x, gl_FragCoord.y), 0);
+	vec4 outColor2 = texelFetch(prevScreenTexture, ivec2(gl_FragCoord.x, gl_FragCoord.y), 0);
+	
+	FragColor = (outColor1 * int(y_even) * int(x_even) + outColor1 * int(!y_even) * int(!x_even) +
+				outColor2 * int(y_even) * int(!x_even) + outColor2 * int(!y_even) * int(x_even)) * int(isEvenFrame) + 
+				(outColor1 * int(y_even) * int(!x_even) + outColor1 * int(!y_even) * int(x_even) +
+	            outColor2 * int(y_even) * int(x_even) + outColor2 * int(!y_even) * int(!x_even)) * int(!isEvenFrame);
+	
+	
+	/*
+	if (y_even == !isEvenFrame) 
+	{
+		if (x_even == isEvenFrame) 
 		{
-			if (mod(floor(gl_FragCoord.x), 2) == 0) 
+			FragColor = texelFetch(prevScreenTexture, ivec2(gl_FragCoord.x, gl_FragCoord.y), 0);
+		}
+		else 
+		{
+			FragColor = texture(screenTexture, TexCoords);
+		}
+		//FragColor = vec4(255, 0, 0, 0);
+	}
+	else 
+	{
+		if (x_even == !isEvenFrame)
+		{
+			FragColor = texelFetch(prevScreenTexture, ivec2(gl_FragCoord.x, gl_FragCoord.y), 0);
+		}
+		else 
+		{
+			FragColor = texture(screenTexture, TexCoords);
+		}
+		//FragColor = vec4(255, 0, 0, 0);
+	}*/
+	/*
+	if (isEvenFrame) {
+		if (y_even == 1) // !isEvenFrame
+		{
+			if (x_even == 0) // isEvenFrame
 			{
 				//FragColor = vec4(255, 0, 0, 0);
 				//FragColor = texture(prevScreenTexture, TexCoords);
@@ -44,12 +85,13 @@ void main()
 			}
 			else 
 			{
-				FragColor = texture(screenTexture, TexCoords);
+				//FragColor = texture(screenTexture, TexCoords);
+				FragColor = texelFetch(screenTexture, ivec2(gl_FragCoord.x, gl_FragCoord.y), 0);
 			}
 		}
 		else 
 		{
-			if (mod(floor(gl_FragCoord.x), 2) == 1) 
+			if (x_even == 1) // isEvenFrame
 			{
 				//FragColor = vec4(255, 0, 0, 0);
 				//FragColor = texture(prevScreenTexture, TexCoords);
@@ -57,14 +99,15 @@ void main()
 			}
 			else 
 			{
-				FragColor = texture(screenTexture, TexCoords);
+				//FragColor = texture(screenTexture, TexCoords);
+				FragColor = texelFetch(screenTexture, ivec2(gl_FragCoord.x, gl_FragCoord.y), 0);
 			}
 		}
 	}
 	else {
-		if (mod(floor(gl_FragCoord.y), 2) == 0)
+		if (y_even == 0)
 		{
-			if (mod(floor(gl_FragCoord.x), 2) == 0) 
+			if (x_even == 0) 
 			{
 				//FragColor = vec4(255, 0, 0, 0);
 				//FragColor = texture(prevScreenTexture, TexCoords);
@@ -72,12 +115,13 @@ void main()
 			}
 			else 
 			{
-				FragColor = texture(screenTexture, TexCoords);
+				//FragColor = texture(screenTexture, TexCoords);
+				FragColor = texelFetch(screenTexture, ivec2(gl_FragCoord.x, gl_FragCoord.y), 0);
 			}
 		}
 		else 
 		{
-			if (mod(floor(gl_FragCoord.x), 2) == 1) 
+			if (x_even == 1) 
 			{
 				//FragColor = vec4(255, 0, 0, 0);
 				//FragColor = texture(prevScreenTexture, TexCoords);
@@ -85,10 +129,12 @@ void main()
 			}
 			else 
 			{
-				FragColor = texture(screenTexture, TexCoords);
+				//FragColor = texture(screenTexture, TexCoords);
+				FragColor = texelFetch(screenTexture, ivec2(gl_FragCoord.x, gl_FragCoord.y), 0);
 			}
 		}
-	}
+	}*/
 	//FragColor = vec4(abs(gl_FragCoord.x)/1000, abs(gl_FragCoord.y)/1000, 0, 0);
 	//FragColor = texture(screenTexture, TexCoords);
+	//FragColor = texelFetch(screenTexture, ivec2(gl_FragCoord.x, gl_FragCoord.y), 0);
 }
